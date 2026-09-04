41a7d2cfa33865b44316137d5b22b9230f7369dd
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2026-09-04 12:00:37 +0530

    feat: conditionally render study tools based on showStudyTools flag

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,9 +4,13 @@ import VoiceReader from './VoiceReader.vue'
 import OllamaAssistant from './OllamaAssistant.vue'
 import './style.css'
 
+const showStudyTools = false
+
 export default {
   extends: DefaultTheme,
   Layout: () => h(DefaultTheme.Layout, null, {
-    'doc-before': () => h('div', { class: 'study-tools' }, [h(VoiceReader), h(OllamaAssistant)]),
+    'doc-before': () => showStudyTools
+      ? h('div', { class: 'study-tools' }, [h(VoiceReader), h(OllamaAssistant)])
+      : null,
   }),
 }
